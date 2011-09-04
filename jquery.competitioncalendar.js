d8819d72e2814ed07df5d3429baf4b873641f0b8
raultm <raultm@raultm-Satellite-A210.(none)>
2011-09-04 11:09:12 +0200

    First green. Checking Default Values

---
+++ jquery.competitioncalendar.js
@@ -0,0 +1,12 @@
+(function($) {
+   var competitionCalendar = $.competitionCalendar = {};
+    
+   competitionCalendar.defaultOptions = {
+      teamsLength : 20
+   };
+                    
+   $.fn.competitionCalendar = function(options) {
+      return this.each(function() {            
+      });
+   };    
+})(jQuery);
