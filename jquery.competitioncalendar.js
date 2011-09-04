36d33a6eb59fef12403fb24c9fadb8653dc02ce7
raultm <raultm@raultm-Satellite-A210.(none)>
2011-09-04 12:11:50 +0200

    Green Test - Scaffold Team Names' Insertion

--- jquery.competitioncalendar.js
+++ jquery.competitioncalendar.js
@@ -5,15 +5,37 @@
       teamsLength : 20
    };
                     
+   $.fn.competitionCalendar = function(options){
+   	competitionCalendar.overrideOptions(options);                    
+ 
+   	return this;
+   };
+
    competitionCalendar.options = null;
     
    competitionCalendar.overrideOptions = function(options) {
       competitionCalendar.options = $.extend({}, competitionCalendar.defaultOptions, options);
    }; 
 
-   $.fn.competitionCalendar = function(options){
-   	competitionCalendar.overrideOptions(options);                    
- 
-   	return this;
+   competitionCalendar.scaffoldTeamNamesInsertion = function(element) {
+	
+	var nameInputs = '';
+	var teamsLength = competitionCalendar.options.teamsLength;
+
+	for(position = 0; position<teamsLength; position++ ){
+		frontendPosition = position + 1;
+		nameInputs += 
+			"<div class='team-name-input-div'>"
+			    + "<label for='team-name-input-" + position + "'>Team Name " + frontendPosition + " : <label>"			
+			    + "<input type='text' name='team-name-input-" + position + "' class='team-name-input' value='Team " + frontendPosition + "'/>"
+			+ "</div>"	
+	}
+
+	var html = 
+		  "<div id='team-names-insertion'>"
+			+ nameInputs
+	        + "</div>";
+		                                  
+	tabs = $(html).appendTo(element).find('#competition-calendar-team-names-insertion');
    };
 })(jQuery);
