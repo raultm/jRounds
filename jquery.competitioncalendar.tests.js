823b7588f7174a5a81c9de5161dc9ea010a52373
raultm <raultm@raultm-Satellite-A210.(none)>
2011-09-04 11:14:15 +0200

    Test created - Override Default Options

--- jquery.competitioncalendar.tests.js
+++ jquery.competitioncalendar.tests.js
@@ -3,3 +3,11 @@ module("Competition Calendar");
 test("Default Options", function() {
     same($.competitionCalendar.defaultOptions.teamsLength, 20);
 });
+
+test("Override Default Options", function() {
+    $.competitionCalendar.overrideOptions({
+        teamsLength : 10,
+    });
+    
+    same($.competitionCalendar.defaultOptions.teamsLength, 20);   
+});
