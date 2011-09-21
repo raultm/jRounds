cf60c71ed6ac298de15018302996e2704bd8e9ce
Raul Tierno <raultm@gmail.com>
2011-09-21 17:40:15 +0200

    jRounds js & tests.js

---
+++ jquery.jrounds.js
@@ -0,0 +1,250 @@
+(function($) {
+   var jRounds = $.jRounds = {};
+   
+   jRounds.defaultOptions = {
+      	  teamsLength : 20
+   };
+                    
+   $.fn.jRounds = function(options){
+   	jRounds.overrideOptions(options);                    
+ 	return this;
+   };
+
+   jRounds.options = null;
+   jRounds.structure = {
+	  'settings' : 
+		{ "id" : 'settings-insertion' }
+	, 'names'    : 
+		{ 'id' : 'team-names-insertion' }
+	, 'output'   : 
+		{ 'id' : 'output-fixtures'
+		, 'options' : 
+			{ 'plain' : { 'id' : 'plain-text'}
+			, 'verbose' : { 'id' : 'verbose'}
+		}
+	}
+   }
+
+   jRounds.classes = {
+	  'menuItem' 	: "menu-element" 
+	, 'settingDiv'  : "setting-option-div"
+	, 'teamNameDiv' : "team-name-input-div"
+	, 'weekContent' : "week-content"
+	, 'weekTitle'   : "week-title"
+	, 'matchContent': "match-content"
+	, 'teamName'	: "team-name"
+	, 'teamLocal'	: "team-local"
+	, 'teamVisitor'	: "team-visitor"
+	, 'weekFixtures': "week-fixtures"
+	, 'weekName'	: "week-name"
+	, 'competitionFixture'	: "competition-fixture"
+	, 'competitionName'	: "competition-name"
+   }
+  
+   jRounds.overrideOptions = function(options) {
+      jRounds.options = $.extend({}, jRounds.defaultOptions, options);
+   }; 
+
+   jRounds.showContent = function(element){
+	structure = jRounds.structure;
+	elementSelected = element;
+	$.each(structure,function( contentIndex, contentIdentifier ) {
+	    if(contentIdentifier.id == element)
+		$('#' + contentIdentifier.id).show();
+	    else
+		$('#' + contentIdentifier.id).hide();
+	});
+   }
+   
+   jRounds.addMenuFunctionality = function(){
+	$('.menu-element').click(function(){
+  	    var id = $(this).attr('id');
+	    var divId = id.replace('menu-', '');
+	    var structure = jRounds.structure;
+	    $.each(structure,function( contentIndex, contentIdentifier ) {
+		console.log(contentIdentifier.id + " == " + structure[divId].id);
+	    	if(contentIdentifier.id == structure[divId].id)
+		    $('#' + contentIdentifier.id).show();
+	    	else
+		    $('#' + contentIdentifier.id).hide();
+	    });
+	});
+   }
+
+   jRounds.addChangeTeamNumberFunctionality = function(){
+   	$('input[name="teamsLength"]').change(function(){
+   		var value = $(this).val();
+  	    var options = { teamsLength : value }
+  	    jRounds.overrideOptions(options)
+  	    console.log(options);
+  	    $('#qunit-target-test').html('');
+  	    jRounds.scaffoldModule( $('#qunit-target-test'));
+	});
+   }
+
+   jRounds.scaffoldModule = function(element){
+	var html = "<div id='competition-calendar'></div>"; 
+	var competitionId = '#competition-calendar';
+	var plainTextId = '#' + jRounds.structure.output.options.plain.id;
+	var verboseId = '#' + jRounds.structure.output.options.verbose.id;
+	$(element).append(html);
+	jRounds.scaffoldMenu($(competitionId));
+  	jRounds.scaffoldSettingsInsertion($(competitionId));
+	jRounds.scaffoldTeamNamesInsertion($(competitionId));
+	jRounds.scaffoldOutputView($(competitionId));
+	teams = jRounds.getTeamNames();
+	jRounds.showFixtures(teams, $(plainTextId));
+	jRounds.showFixtures(teams, $(verboseId));
+   }
+
+   jRounds.scaffoldOutputView = function(element) {
+    	var html = 
+		  "<div id='" + jRounds.structure.output.id + "'>"
+			+ "<div id='" + jRounds.structure.output.options.plain.id + "'></div>"
+			+ "<textarea id='" + jRounds.structure.output.options.verbose.id + "'></div>"
+	        + "</div>";
+		                                  
+	$(element).append(html);
+   }
+
+   jRounds.scaffoldMenu = function(element){
+	var menuLis = "";
+	for(structureElement in jRounds.structure){
+            menuLis+= "<li class='" + jRounds.classes.menuItem + "' id='menu-" + structureElement + "'>" + structureElement + "</li>";
+	}
+	var menu =   "<div id='competition-menu'>"
+		   	+ "<ul>"
+			    + menuLis
+			+ "</ul>"
+		   + "</div>"
+	;
+	$(element).append(menu);
+	jRounds.addMenuFunctionality();
+   }
+
+   jRounds.scaffoldSettingsInsertion = function(element) {
+        var settingOption = '';var teamsLength = jRounds.options.teamsLength;
+	settingOption += 
+			"<div class='" + jRounds.classes.settingDiv + "'>"
+			    + "<label for='setting-option-teams-length'>Number of Teams : <label>"			
+			    + "<input type='text' name='teamsLength' class='setting-option' value='" + teamsLength + "'/>"
+			+ "</div>";
+	var html = 
+		  "<div id='" + jRounds.structure.settings.id + "'>"
+			+ settingOption
+	        + "</div>";
+	$(element).append(html);
+   }
+
+   jRounds.getSettings = function(){
+	var settingInputs = $('.setting-option');
+	var settings = {}
+	var settingJSON = {}
+	$.each(settingInputs,function( index, settingInput ) { 
+		settingName = $(settingInput).attr('name');
+		settingValue = $(settingInput).val();
+		settingJSON[settingName] = settingValue;
+
+		$.extend(settings, settingJSON);
+	});
+	return settings;
+   }
+
+   jRounds.scaffoldTeamNamesInsertion = function(element) {
+	var nameInputs = '';
+	var teamsLength = jRounds.options.teamsLength;
+	for(position = 0; position<teamsLength; position++ ){
+		frontendPosition = position + 1;
+		nameInputs += 
+			"<div class='" + jRounds.classes.teamNameDiv +"'>"
+			    + "<label for='team-name-input-" + position + "'>Team Name " + frontendPosition + " : <label>"			
+			    + "<input type='text' name='team-name-input-" + position + "' class='team-name-input' value='Team " + frontendPosition + "'/>"
+			+ "</div>";
+	}
+	var html = 
+		  "<div id='" + jRounds.structure.names.id + "'>"
+			+ nameInputs
+	        + "</div>";
+	$(element).append(html);
+	jRounds.addChangeTeamNumberFunctionality();
+   };
+
+   jRounds.getTeamNames = function(){
+	teamNamesInputs = $('.team-name-input');
+	teamNames = $.map(teamNamesInputs, function(input, index){ return $(input).val(); });
+        return teamNames;
+   }
+
+   jRounds.showFixtures = function( teams, element ){
+	var fixtures = jRounds.getFixtures(teams);
+	fixturesHtml = jRounds.parseFixtureJson2FixtureOutput(fixtures);
+	$(element).html(fixturesHtml);	
+    }
+
+   jRounds.getFixtures = function(teams) {
+	if(!teams){ return null; }
+	teamsCount = 0;for(team in teams){teamsCount++;}
+	if(teamsCount == 0){ return null; }
+	fixtures = {'weeks' : {} };
+	rotatedTeams = $.extend(true, {}, teams);
+	halfTeamsCount = teamsCount/2;
+	for(week=0; week<(teamsCount-1)*2; week++){
+		frontEndWeek = week + 1;
+		fixtures.weeks[frontEndWeek] = { "matches" : {} };
+		rotatedTeams = this.rotateTeamsJSON(rotatedTeams);
+		for(rotatedTeamsIndex=0; rotatedTeamsIndex < halfTeamsCount; rotatedTeamsIndex++){
+			
+			if(rotatedTeamsIndex==0 && (week%2)==0)	{ local   = rotatedTeamsIndex; 	visitor = teamsCount - rotatedTeamsIndex - 1; }
+			else if(rotatedTeamsIndex==0)		{ visitor = rotatedTeamsIndex;	local   = teamsCount - rotatedTeamsIndex - 1; 	 }
+			else if((rotatedTeamsIndex%2)==0)	{ local   = rotatedTeamsIndex; 	visitor = teamsCount - rotatedTeamsIndex - 1; }
+			else					{ visitor = rotatedTeamsIndex;  local   = teamsCount - rotatedTeamsIndex - 1; 	 }
+			fixtures.weeks[frontEndWeek].matches[rotatedTeamsIndex] = {
+				  'local' : rotatedTeams[local]
+				, 'visitor' : rotatedTeams[visitor]
+			};
+		}
+	}
+	return fixtures;
+   };
+
+   jRounds.rotateTeamsJSON = function(teamsToRotate){
+	teamsRotates = $.extend(true, {}, teamsToRotate);
+	sizeOfTeamsToRotate = 0;for(team in teamsToRotate){sizeOfTeamsToRotate++;}
+	elementToRotate_1 = teamsRotates[sizeOfTeamsToRotate-2];
+	elementToRotate_2 = {};
+	for(index=0;index<(sizeOfTeamsToRotate-1);index++){
+		elementToRotate_2 = teamsRotates[index];
+		teamsRotates[index] = elementToRotate_1;
+		elementToRotate_1 = elementToRotate_2;
+	}
+	return teamsRotates;
+   }
+   
+   jRounds.parseFixtureJson2FixtureOutput = function(fixtureJson){
+	
+	fixtureOutput	= "";
+	weeksOutput 	= "";
+	matchesOutput 	= "";
+	$.each(fixtureJson.weeks,function( weekIndex, week ) { 
+	    matchesOutput = "";
+	    $.each(week.matches,function( matchIndex, match ) { 
+		matchesOutput+= 
+		  "<div class='match-fixtures'>"
+	            + "<span class='" + jRounds.classes.teamLocal + "'>" + match.local + "</span>"
+		    + "<span class='" + jRounds.classes.teamVisitor + "'>" + match.visitor + "</span>"
+		+ "</div>"		    
+	    });
+	    weeksOutput += 
+	      "<div class='" + jRounds.classes.weekFixtures + "'>"
+		+ "<span class='" + jRounds.classes.weekName + "'>Week " + weekIndex + "</span>"
+		+ matchesOutput
+	    + "</div>"
+	});
+	fixtureOutput = "<div class='" + jRounds.classes.competitionFixture + "'>"
+	    + "<span class='" + jRounds.classes.competitionName + "'>Competition</span>"
+	    + weeksOutput
+	+ "</div>"
+        ;
+	return fixtureOutput;
+   }
+})(jQuery);
---
+++ jquery.jrounds.tests.js
@@ -0,0 +1,370 @@
+module("Beginning");
+
+var targetId = "#qunit-target";
+var testTargetId = "#qunit-target-test";
+var teams2 = {
+    	  '0' : 'team1'
+ 	, '1' : 'team2'
+}
+var fixtures2 = {
+	  'weeks' : { 
+		  '1' : { 'matches' : { '0' : {  'local' : teams2[0], 'visitor' : teams2[1] } } } 
+		, '2' : { 'matches' : { '0' : {  'local' : teams2[1], 'visitor' : teams2[0] } } } 
+	}
+}
+
+var teams4 = {
+    	  '0' : 'team1'
+ 	, '1' : 'team2'
+	, '2' : 'team3'
+	, '3' : 'team4'
+}
+var fixtures4 = {
+	"weeks": {
+	    "1": {
+	      "matches": { "0": { "local": teams4[2], "visitor": teams4[3]}, "1": { "local": teams4[1], "visitor": teams4[0] } }
+	    },
+	    "2": {
+	      "matches": { "0": { "local": teams4[3], "visitor": teams4[1]}, "1": { "local": teams4[0], "visitor": teams4[2] } }
+	    },
+	    "3": {
+	      "matches": { "0": { "local": teams4[0], "visitor": teams4[3]}, "1": { "local": teams4[2], "visitor": teams4[1] } }
+	    },
+	    "4": {
+	      "matches": { "0": { "local": teams4[3], "visitor": teams4[2]}, "1": { "local": teams4[1], "visitor": teams4[0] } }
+	    },
+	    "5": {
+	      "matches": { "0": { "local": teams4[1], "visitor": teams4[3]}, "1": { "local": teams4[0], "visitor": teams4[2] } }
+	    },
+	    "6": {
+	      "matches": { "0": { "local": teams4[3], "visitor": teams4[0]}, "1": { "local": teams4[2], "visitor": teams4[1] } }
+	    }
+    }
+}
+
+
+test("Default Options", function() {
+    same($.jRounds.defaultOptions.teamsLength, 20);
+});
+
+test("Override Default Options", function() {
+    $.jRounds.overrideOptions({
+        teamsLength : 10
+    });
+    
+    same($.jRounds.options.teamsLength, 10);   
+});
+
+test("Calling Plugin Override Options", function() {
+   $(targetId).jRounds({
+      testOption : "Almendralejo"
+   });
+    
+   same($.jRounds.options.teamsLength, 20); 
+   same($.jRounds.options.testOption, "Almendralejo");    
+});
+
+module("UI", {
+    setup: function() {    
+ 	$(targetId).empty();  
+	$.jRounds.overrideOptions({});
+    },
+    teardown: function() {}
+});
+
+test("Scaffold Menu", function() {
+   $.jRounds.scaffoldMenu($(targetId));
+   structure = $.extend(true, {},$.jRounds.structure);
+   countElements = 0; for(properties in structure){ countElements++; }
+   same($('.menu-element').size(), countElements, "We have " + countElements + " '.menu-element'");
+});
+
+test("Scaffold Output View", function() {
+   $.jRounds.scaffoldOutputView($(targetId));
+   ok($(targetId).find('#output-fixtures').length, 'Prepare to view Output');
+   same($('#plain-text').size(), 1, "We have '#plain-text'");
+   same($('#verbose').size(), 1, "We have '#verbose'");
+});
+
+test("Scaffold Settings' Form", function() {
+   $.jRounds.scaffoldSettingsInsertion($(targetId));
+   ok($(targetId).find('#settings-insertion').length, 'Prepare to introduce Settings');
+   same($('.setting-option-div').size(), 1, "We have 1 '.setting-option-div'");
+   ok($(targetId).find('.setting-option-div [name$="teamsLength"]').length, 'Exists Teams Length Setting');
+});
+
+test("After Scaffold Settings' Form get Settings in JSONObject", function() {
+   $.jRounds.overrideOptions({ teamsLength : 4 });
+   $.jRounds.scaffoldSettingsInsertion($(targetId));
+	
+   settings = $.jRounds.getSettings();
+   
+   expectedSettings = {
+	"teamsLength" : "4"
+   };
+
+   same(settings, expectedSettings, "Checked Setting Options");
+
+});
+
+test("Scaffold Team Names' Form", function() {
+   $.jRounds.scaffoldTeamNamesInsertion($(targetId));
+   ok($(targetId).find('#team-names-insertion').length, 'Prepare to introduce Team Names');
+   same($('.team-name-input-div').size(), 20, "We have 20 '.team-name-input-div'");
+});
+
+test("After Scaffold Team Names' Insertion get Teams Name in JSONObject", function() {
+   $.jRounds.overrideOptions({ teamsLength : 4 });
+   $.jRounds.scaffoldTeamNamesInsertion($(targetId));
+	
+   teamNames = $.jRounds.getTeamNames();
+   
+   expectedTeamNames = ["Team 1", "Team 2", "Team 3", "Team 4"];
+
+   same(teamNames, expectedTeamNames, "We've received 4 Team Names");
+
+});
+
+test("Scaffold Show Fixtures (2 teams)", function() {
+   $.jRounds.overrideOptions({ teamsLength : 2 });
+   $.jRounds.scaffoldTeamNamesInsertion($(targetId));
+   teamNames = $.jRounds.getTeamNames();
+   
+   $.jRounds.showFixtures( teamNames, targetId );
+   ok($(targetId).find('.competition-fixture').length, 'Exists div.competition-fixture');
+   same($('.week-fixtures').size(), 2, "We have 2 '.week-fixtures'");
+   same($('.match-fixtures').size(), 2, "We have 2 '.match-fixtures'");	
+});
+
+test("Scaffold Show Fixtures (4 teams)", function() {
+   $.jRounds.overrideOptions({ teamsLength : 4 });
+   $.jRounds.scaffoldTeamNamesInsertion($(targetId));
+   teamNames = $.jRounds.getTeamNames();
+   
+   $.jRounds.showFixtures( teamNames, targetId );
+   ok($(targetId).find('.competition-fixture').length, 'Exists div.competition-fixture');
+   same($('.week-fixtures').size(), 6, "We have 6 '.week-fixtures'");
+   same($('.match-fixtures').size(), 12, "We have 12 '.match-fixtures'");	
+});
+
+test("Scaffold Complete Module", function() {
+   $.jRounds.overrideOptions({ teamsLength : 4 });
+   $.jRounds.scaffoldModule($(targetId));
+   
+   
+   ok($(targetId).find('#competition-calendar').length, 'Main Div Created');
+   ok($(targetId).find('#competition-menu').length, 'Menu Div Created');
+   ok($(targetId).find('#settings-insertion').length, 'Prepare to introduce Settings');
+   same($('.setting-option-div').size(), 1, "We have 1 '.setting-option-div'");
+   ok($(targetId).find('.setting-option-div [name$="teamsLength"]').length, 'Exists Teams Length Setting');
+   same($('.team-name-input-div').size(), 4, "We have 4 '.team-name-input-div'");
+   same($('.week-fixtures').size(), 6, "We have 6 '.week-fixtures'");
+   same($('.match-fixtures').size(), 12, "We have 12 '.match-fixtures'");
+   ok($(targetId).find('#output-fixtures').length, 'Prepare to view Output');
+   same($('#plain-text').size(), 1, "We have '#plain-text'");
+   same($('#verbose').size(), 1, "We have '#verbose'");
+});
+
+test("showContent", function() {
+	$.jRounds.scaffoldModule($(testTargetId));
+	structure = $.jRounds.structure;
+	countMenuElements = 0; for(element in structure){countMenuElements++;}
+	ok(countMenuElements, "El menú tiene al menos un elemento");
+	
+	contentSelected = structure.settings.id;
+	$.jRounds.showContent(contentSelected);
+	$.each(structure,function( contentIndex, contentIdentifier ) {
+	    if(contentIdentifier.id == contentSelected)
+		ok(!$('#' + contentIdentifier.id).is(':hidden'), "El contenido no está oculto");
+	    else
+		ok(!$('#' + contentIdentifier.id).is(':visible'), contentIdentifier + " está oculto");
+	});
+	
+});
+
+module("Events", {
+    setup: function() {    
+ 	$(targetId).empty();  
+	$(testTargetId).empty();  
+	$.jRounds.overrideOptions({});
+    },
+    teardown: function() {}
+});
+
+test("Add Menu Funcionality - Click Output", function() {
+   $.jRounds.overrideOptions({ teamsLength : 4 });
+   $.jRounds.scaffoldModule($(testTargetId));
+   $.jRounds.addMenuFunctionality();
+
+   var structure = $.jRounds.structure;
+   var menuId = "#menu-output";
+   var divId = structure.output.id;
+    
+   $(menuId).click();
+   $.each(structure,function( contentIndex, contentIdentifier ) {
+        if(contentIdentifier.id == divId)
+	    ok(!$('#' + contentIdentifier.id).is(':hidden'), contentIdentifier.id + " no está oculto");
+     	else
+	    ok(!$('#' + contentIdentifier.id).is(':visible'), contentIdentifier.id + " está oculto");
+   });
+});
+
+test("Add Menu Funcionality - Click Names", function() {
+   $.jRounds.overrideOptions({ teamsLength : 4 });
+   $.jRounds.scaffoldModule($(testTargetId));
+   $.jRounds.addMenuFunctionality();
+
+   structure = $.jRounds.structure;
+   menuId = "#menu-names";
+   divId = structure.names.id;
+   
+   $(menuId).click();
+   $.each(structure,function( contentIndex, contentIdentifier ) {
+	if(contentIdentifier.id == divId)
+	    ok(!$('#' + contentIdentifier.id).is(':hidden'), contentIdentifier.id + " no está oculto");
+     	else
+	    ok(!$('#' + contentIdentifier.id).is(':visible'), contentIdentifier.id + " está oculto");
+   });
+});
+
+test("Add Refresh Funcionality - Change Team Number", function() {
+   $.jRounds.overrideOptions({ teamsLength : 8 });
+   $.jRounds.scaffoldModule($(testTargetId));
+   $('input[name="teamsLength"]').val(6).trigger('change');
+   same($('.team-name-input-div').size(), 6, "We have 6 '.team-name-input-div'");
+   
+});
+
+module("Core");
+
+test("rotateTeamsJSON Function receive teams param empty", function() {
+    teams = {};
+    same($.jRounds.rotateTeamsJSON(teams), {});
+});
+
+test("rotateTeamsJSON Function receive teams param(size 2)", function() {
+    expectedTeams = {
+    	  '0' : 'team1'
+ 	, '1' : 'team2'
+    };
+    same($.jRounds.rotateTeamsJSON(teams2), expectedTeams, "With Elements we must have the same JSON");
+});
+test("rotateTeamsJSON Function receive teams param(size 4)", function() {
+    expectedTeams = {
+    	  '0' : 'team3'
+ 	, '1' : 'team1'
+	, '2' : 'team2'
+	, '3' : 'team4'
+    };
+    same($.jRounds.rotateTeamsJSON(teams4), expectedTeams, "With Four elements we must see hot the first three elements have changed");
+});
+
+test("getFixtures Function receive teams param empty", function() {
+    teams = {};
+    same($.jRounds.getFixtures(teams), null);
+});
+
+test("getFixtures Function receive teams param(Size 2)", function() {
+    same($.jRounds.getFixtures(teams2), fixtures2);
+});
+
+test("getFixtures Function receive teams param(Size 4)", function() {
+   same($.jRounds.getFixtures(teams4), fixtures4, "Fixtures Ok with 4 teams and 2 legs");
+});
+
+test("parseFixtureJson2FixtureOutput (Teams Size 2)", function() {
+    expectedString = "<div class='competition-fixture'>"
+	    + "<span class='competition-name'>Competition</span>"
+	    + "<div class='week-fixtures'>"
+		+ "<span class='week-name'>Week 1</span>"
+		+ "<div class='match-fixtures'>"
+	            + "<span class='team-local'>" + teams2[0] + "</span>"
+		    + "<span class='team-visitor'>" + teams2[1] + "</span>"
+		+ "</div>"
+	    + "</div>"
+	    + "<div class='week-fixtures'>"
+		+ "<span class='week-name'>Week 2</span>"
+		+ "<div class='match-fixtures'>"
+	            + "<span class='team-local'>" + teams2[1] + "</span>"
+		    + "<span class='team-visitor'>" + teams2[0] + "</span>"
+		+ "</div>"
+	    + "</div>"
+	+ "</div>"
+    ;
+    same($.jRounds.parseFixtureJson2FixtureOutput(fixtures2), expectedString, "Json converted to String Ok!");
+});
+
+test("parseFixtureJson2FixtureOutput (Teams Size 4)", function() {
+    var expectedString = "<div class='competition-fixture'>"
+	    + "<span class='competition-name'>Competition</span>"
+	    + "<div class='week-fixtures'>"
+		+ "<span class='week-name'>Week 1</span>"
+		+ "<div class='match-fixtures'>"
+	            + "<span class='team-local'>" + teams4[2] + "</span>"
+		    + "<span class='team-visitor'>" + teams4[3] + "</span>"
+		+ "</div>"
+		+ "<div class='match-fixtures'>"
+	            + "<span class='team-local'>" + teams4[1] + "</span>"
+		    + "<span class='team-visitor'>" + teams4[0] + "</span>"
+		+ "</div>"
+	    + "</div>"
+	    + "<div class='week-fixtures'>"
+		+ "<span class='week-name'>Week 2</span>"
+		+ "<div class='match-fixtures'>"
+	            + "<span class='team-local'>" + teams4[3] + "</span>"
+		    + "<span class='team-visitor'>" + teams4[1] + "</span>"
+		+ "</div>"
+		+ "<div class='match-fixtures'>"
+	            + "<span class='team-local'>" + teams4[0] + "</span>"
+		    + "<span class='team-visitor'>" + teams4[2] + "</span>"
+		+ "</div>"
+	    + "</div>"
+	    + "<div class='week-fixtures'>"
+		+ "<span class='week-name'>Week 3</span>"
+		+ "<div class='match-fixtures'>"
+	            + "<span class='team-local'>" + teams4[0] + "</span>"
+		    + "<span class='team-visitor'>" + teams4[3] + "</span>"
+		+ "</div>"
+		+ "<div class='match-fixtures'>"
+	            + "<span class='team-local'>" + teams4[2] + "</span>"
+		    + "<span class='team-visitor'>" + teams4[1] + "</span>"
+		+ "</div>"
+	    + "</div>"
+	    + "<div class='week-fixtures'>"
+		+ "<span class='week-name'>Week 4</span>"
+		+ "<div class='match-fixtures'>"
+	            + "<span class='team-local'>" + teams4[3] + "</span>"
+		    + "<span class='team-visitor'>" + teams4[2] + "</span>"
+		+ "</div>"
+		+ "<div class='match-fixtures'>"
+	            + "<span class='team-local'>" + teams4[1] + "</span>"
+		    + "<span class='team-visitor'>" + teams4[0] + "</span>"
+		+ "</div>"
+	    + "</div>"
+	    + "<div class='week-fixtures'>"
+		+ "<span class='week-name'>Week 5</span>"
+		+ "<div class='match-fixtures'>"
+	            + "<span class='team-local'>" + teams4[1] + "</span>"
+		    + "<span class='team-visitor'>" + teams4[3] + "</span>"
+		+ "</div>"
+		+ "<div class='match-fixtures'>"
+	            + "<span class='team-local'>" + teams4[0] + "</span>"
+		    + "<span class='team-visitor'>" + teams4[2] + "</span>"
+		+ "</div>"
+	    + "</div>"
+	    + "<div class='week-fixtures'>"
+		+ "<span class='week-name'>Week 6</span>"
+		+ "<div class='match-fixtures'>"
+	            + "<span class='team-local'>" + teams4[3] + "</span>"
+		    + "<span class='team-visitor'>" + teams4[0] + "</span>"
+		+ "</div>"
+		+ "<div class='match-fixtures'>"
+	            + "<span class='team-local'>" + teams4[2] + "</span>"
+		    + "<span class='team-visitor'>" + teams4[1] + "</span>"
+		+ "</div>"
+	    + "</div>"	    
+	+ "</div>"
+    ;
+
+   same($.jRounds.parseFixtureJson2FixtureOutput(fixtures4), expectedString, "Json converted to String Ok!");
+});
