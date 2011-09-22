91937ec1401381f162df0c734dfb15cf8fd697d3
Raul Tierno <raultm@gmail.com>
2011-09-22 23:19:16 +0200

    Test - getTeamName

--- jquery.jrounds.tests.js
+++ jquery.jrounds.tests.js
@@ -462,6 +462,10 @@ test("isValidTeamNumber Value 'a' false", function(){
     same($.jRounds.isValidTeamNumber('a'),false, "a is not a number");
 });
 
+test("getTeamName 1", function(){
+    same($.jRounds.getTeamName(),null, "Empty params return null");
+});
+
 
 
 
